c716e4a73eb6597a7b0efd96fc972c39d609c544
wufeng.jiang <jwf1990@126.com>
2021-04-14 08:00:25 +0800

    re regernate icon

--- 64X64.bat
+++ 64X64.bat
@@ -1,1 +1,6 @@
-gen_png.bat 64X64 64 64
+start gen_png.bat 36X36    36  36
+start gen_png.bat 48X48    48  48
+start gen_png.bat 72X72    72  72
+start gen_png.bat 96X96    96  96
+start gen_png.bat 144X144 144  144
+start gen_png.bat 192X192 192  192
