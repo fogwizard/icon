989a611f4ed3b4f0a5834e6d0e118549fbd07ea7
wufeng.jiang <jwf1990@126.com>
2021-04-15 22:53:24 +0800

    update usb icon

--- Background.bat
+++ Background.bat
@@ -2,3 +2,4 @@ start gen_png.bat 600X1024    600 1024  bg_black.svg
 start gen_png.bat 600X1024    600 1024  bg_blue.svg
 start gen_png.bat 600X1024    600 1024  bg_blue_light.svg
 start gen_png.bat 600X1024    600 1024  bg_red.svg
+start gen_png.bat 100X60       100  60     arrow_left.svg
